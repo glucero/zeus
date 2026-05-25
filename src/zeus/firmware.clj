eff6245af837c602560afde00e595c399df365ca
Gino Lucero <glucero@gmail.com>
2026-05-25 12:46:18 -0600

    added firmware/download with streaming and progress

--- src/zeus/firmware.clj
+++ src/zeus/firmware.clj
@@ -1,5 +1,6 @@
 (ns zeus.firmware
   (:require [babashka.http-client :as http]
+            [clojure.java.io :as io]
             [clojure.string :as str]))
 
 (def ps3-updatelist-url
@@ -11,6 +12,11 @@
   [url]
   (-> (http/get url) :body))
 
+(defn fetch-stream
+  "GET `url` as a stream. Pulled out so tests can redef it."
+  [url]
+  (http/get url {:as :stream}))
+
 (defn ps3-pup-url
   "Resolve the URL of the current PS3 firmware PUP by parsing Sony's
    update-list endpoint. Returns nil if no PS3UPDAT.PUP line is found."
@@ -20,3 +26,32 @@
                   (filter (fn [l] (re-find #"PS3UPDAT\.PUP" l)))
                   first)]
     (some->> line (re-find #"CDN=([^;]+)") second)))
+
+(defn- stream-to-file
+  [^java.io.InputStream in ^java.io.File file total chunk-size progress-fn]
+  (with-open [out (io/output-stream file)]
+    (let [buffer (byte-array chunk-size)]
+      (loop [downloaded 0]
+        (let [n (.read in buffer)]
+          (if (neg? n)
+            downloaded
+            (do (.write out buffer 0 n)
+                (let [next-total (+ downloaded n)]
+                  (when (and total progress-fn) (progress-fn next-total total))
+                  (recur next-total)))))))))
+
+(defn download
+  "Download `url` to `file`. Streams via a `.partial` sibling and
+   renames on success. Returns the file."
+  [url ^java.io.File file {:keys [progress-fn chunk-size]
+                           :or {chunk-size 65536}}]
+  (io/make-parents file)
+  (let [{:keys [body headers]} (fetch-stream url)
+        total (some-> (or (get headers "content-length")
+                          (get headers "Content-Length"))
+                      Long/parseLong)
+        partial (io/file (str (.getPath file) ".partial"))]
+    (with-open [in body]
+      (stream-to-file in partial total chunk-size progress-fn))
+    (.renameTo partial file)
+    file))
--- test/zeus/firmware_test.clj
+++ test/zeus/firmware_test.clj
@@ -1,6 +1,16 @@
 (ns zeus.firmware-test
   (:require [clojure.test :refer [deftest is testing]]
-            [zeus.firmware :as firmware]))
+            [zeus.firmware :as firmware])
+  (:import (java.io ByteArrayInputStream)))
+
+(defn- temp-dir []
+  (doto (java.io.File/createTempFile "zeus-fw" "")
+    (.delete) (.mkdir) (.deleteOnExit)))
+
+(defn- fake-stream [^String body]
+  (fn [_url]
+    {:body (ByteArrayInputStream. (.getBytes body))
+     :headers {"content-length" (str (.length body))}}))
 
 (def fake-updatelist
   (str "# US\n"
@@ -15,3 +25,21 @@
   (testing "returns nil when no PS3UPDAT.PUP line is present"
     (with-redefs [firmware/fetch-text (fn [_url] "# Empty\n")]
       (is (nil? (firmware/ps3-pup-url))))))
+
+(deftest download
+  (testing "writes body to file and returns the file"
+    (let [d (temp-dir)
+          f (java.io.File. d "TEST.PUP")]
+      (with-redefs [firmware/fetch-stream (fake-stream "FAKE-PUP")]
+        (let [out (firmware/download "http://x" f {})]
+          (is (= f out))
+          (is (= "FAKE-PUP" (slurp out)))))))
+  (testing "calls progress-fn after each chunk with [done total]"
+    (let [d (temp-dir)
+          f (java.io.File. d "TEST.PUP")
+          calls (atom [])
+          progress (fn [done total] (swap! calls conj [done total]))]
+      (with-redefs [firmware/fetch-stream (fake-stream "ABCDEF")]
+        (firmware/download "http://x" f {:progress-fn progress :chunk-size 2}))
+      (is (seq @calls))
+      (is (= [6 6] (last @calls))))))
